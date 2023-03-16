ade906c74d1c6fa0801ba9bc5e9d3f81d2dcfc10
Paulo Andreola <pauloandreola@gmail.com>
2023-03-15 23:20:19 -0300

    feat: config and connect mongodb atlas and config dotenv data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
@@ -10,4 +13,11 @@ app.get('/', (req, res) => {
   res.status(200).json({msg: 'Test'})
 })
 
-app.listen(port, () => (console.log(`Server is running at port => ${port}`)));
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.usyc5sw.mongodb.net/bancoNodejsExpressMongo?retryWrites=true&w=majority`)
+.then(() => {
+  console.log('MongoDB Altas connected!')
+  app.listen(port, () => (console.log(`Server is running at port => ${port}`)));
+})
+.catch((err) => console.log('Not connected at MongoDB Atlas verify above =>',err));
+
+
