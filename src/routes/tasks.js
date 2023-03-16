2a2e2f4b1864ec90f05efd090df0453bbf0f4bee
Paulo Andreola <pauloandreola@gmail.com>
2023-03-16 03:23:22 -0300

    feat: create task post method

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,21 +6,6 @@ router.get('/', (req, res) => {
   res.status(200).json({msg: 'Test'})
 })
 
-router.post('/', async (req, res) => {
-  const { task, finished } = req.body;
-  const person = { task, finished }
-
-  if(!task) {
-    res.status(404).json('Task not found!')
-  }
-
-  try {
-    await Person.create(person)
-  } catch(error) {
-    res.status(500).json('Server error');
-  }
-    res.status(201).json('User created!');
-
-})
+router.post('/', async (req, res) => taskController.create(req, res));
 
 module.exports = router;
