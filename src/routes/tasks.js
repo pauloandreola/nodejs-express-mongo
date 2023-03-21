2a846e653ef0ecffd41b65134ca1509071326eca
Paulo Andreola <pauloandreola@gmail.com>
2023-03-21 11:07:28 -0300

    refactor: clean code, blank lines removed

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,13 +3,9 @@ const router = require('express').Router();
 const taskController = require('../controllers/taskController');
 
 router.delete('/:id', (req, res) => taskController.delete(req, res));
-
 router.get('/', (req, res) => taskController.getAll(req, res));
-
 router.get('/:id', (req, res) => taskController.get(req, res));
-
 router.post('/', (req, res) => taskController.create(req, res));
-
 router.put('/:id', (req, res) => taskController.update(req, res));
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,13 +3,9 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 router.delete('/:id', (req, res) => userController.delete(req, res));
-
 router.get('/', (req, res) => userController.getAll(req, res));
-
 router.get('/:id', (req, res) => userController.get(req, res));
-
-router.post('/', async (req, res) => userController.create(req, res));
-
+router.post('/', (req, res) => userController.create(req, res));
 router.put('/:id', (req, res) => userController.update(req, res));
 
 module.exports = router;
