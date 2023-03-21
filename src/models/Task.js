46bf6dc1d17a4d84b181bd899fabeef07aec170b
Paulo Andreola <pauloandreola@gmail.com>
2023-03-21 15:23:52 -0300

    feat: update task entities with timestamp method

--- src/models/Task.js
+++ src/models/Task.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  updateAt: Date,
-  finished: Boolean,
-});
+    title: String,
+    description: String,
+    createdAt: Date,
+    updateAt: Date,
+    finished: Boolean,
+  },
+  {timestamps: true},
+);
 
 const Task = mongoose.model('Task', taskSchema);
 
