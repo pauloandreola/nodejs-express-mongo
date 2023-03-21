e3e144b1672bde25bc89fa9fe2e8302f9a41a1a3
Paulo Andreola <pauloandreola@gmail.com>
2023-03-21 15:23:23 -0300

    feat: update user entities with timestamp method

--- src/models/User.js
+++ src/models/User.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  createdAt: Date,
-  updateAt: Date,
- },
- {timestamps: true},
+    name: String,
+    email: String,
+    password: String,
+    createdAt: Date,
+    updateAt: Date,
+  },
+  {timestamps: true},
 );
 
 const User = mongoose.model('User', userSchema);
